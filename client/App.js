0d2fc2b790238a34aedb3d0341c4e62dab7439f8
MikeQVo <angelictide@gmail.com>
2019-05-30 09:03:17 -0600

    outlined very top component

--- client/App.js
+++ client/App.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
 import $ from 'jquery';
-import BizHeader from './components/BizHeader.jsx';
-import BizSubHeader from './components/BizSubHeader.jsx';
+import LeftHeader from './components/LeftHeader.jsx';
+import RightHeader from './components/RightHeader.jsx';
+import Map from './components/Map.jsx';
+import Gallery from './components/Gallery.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -28,13 +30,19 @@ class App extends React.Component {
 
   }
 
-
-
   render () {
-    return (<div>
-      <BizHeader info={this.state.businessInfo}/>
-      <BizSubHeader address={this.state.completeAddress}/>
-    </div>)
+    return (
+      <div class="content-container">
+        <div class="biz-header">
+          <LeftHeader info={this.state.businessInfo} />
+          <RightHeader />
+        </div>
+        <div class="biz-sub-header">
+          <Map location={this.state.completeAddress} />
+          <Gallery pictures={this.state.pictures} />
+        </div>
+      </div>
+    )
   }
 }
 
